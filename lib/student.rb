324b238cc8ce88b160109ca213a3677015b8d85c
Eve-Kamau <evelyne.kamau@student.moringaschool.com>
2023-04-07 20:04:24 +0300

    commit tree 07e407080a1e76c694795d72eabd588f90f64035

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,20 @@
-class Student 
+
+require_relative "../lib/user.rb"
+
+class Student < User
+
+    @@knowledge = []
+
+    def initialize
+     @knowledge = knowledge
+    end 
+
+    def learn (article)
+       @@knowledge << article
+    end
+
+    def knowledge
+        @@knowledge
+    end
 
 end
